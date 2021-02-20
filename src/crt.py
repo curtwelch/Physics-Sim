c04f9dd6358c1fbbd4462588d46d2905b7554fbf
curtwelch <curt@kcwc.com>
2021-02-20 17:28:37 -0500

    crt.py unit testing added.

--- src/crt.py
+++ src/crt.py
@@ -1,58 +1,120 @@
 """
     module crt
-    ANSI CRT (VT100) Control
+    ANSI CRT (VT100) Escape Codes
+
+    Only a very few basic function included.
+    See: https://en.wikipedia.org/wiki/ANSI_escape_code
+    for detailed documentation and history of these codes.
 
     2021-2-30 Curt Welch copied from other stuff I've used for years.
 """
 
+from typing import Union
+
 
 def clear():
-    """ Clear screen without moving cursor, """
+    """ Clear screen without moving cursor. """
     print("\033[2J", end='')
 
 
-def clear_and_home(row: int = 0, col: int = 0):
-    """ Clear and move cursor (upper left corner by default) """
+def clear_and_home():
+    """ Clear screen and move cursor to upper left corner. """
     clear()
-    goto(row, col)
+    home()
 
 
-def goto(row: int = 0, col: int = 0):
-    """ Move cursor to row, col (defaults to upper left corner)
-        0,0 is top left corner.
+def home():
+    """
+        Move cursor to top left corner.
+        Alias for goto() or goto(1, 1)
+    """
+    goto(1, 1)
+
+
+def goto(row: int = 1, col: int = 1):
+    """ Position cursor to row, col (defaults to upper left corner)
+        1,1 is top left corner.
         Rows move down lines.
-        Columns move right across characters.
+        Columns move right across screen.
     """
     print(f"\033[{row};{col}H", end='')
 
 
-def mode(mode_str: str = None, m_value: int = None):
-    # TODO finish writing and testing this
+def mode(m: Union[str, int] = 0):
     """
-        Set video mode for characters drawn.
-        Defaults to normal mode -- all special effects turned off.
-        mode_str == NONE -> normal mode -- all off
-        mode_str == "normal" same as NONE
-        mode_str == "bold"
-        mode_str == "underline"
-        mode_str == "blinking"
-        mode_str == "reverse video"
+        Set video mode for characters.
+        Defaults to normal mode.
+        m == 0 or "normal" -- all effects off
+        m == 1 or "bold"
+        m == 2 or "underline"
+        m == 5 or "blinking"
+        m == 7 or "reverse video"
+        For other values of m see:
+        https://en.wikipedia.org/wiki/ANSI_escape_code#SGR
     """
-    if m_value is not None:
-        if mode_str is None:
-            m_value = 0  # normal mode
-        elif mode_str == "normal":
-            m_value = 0
-        elif mode_str == "bold":
-            m_value = 1
-        elif mode_str == "underline":
-            m_value = 2
-        elif mode_str == "blinking":
-            m_value = 5
-        elif mode_str == "reverse video":
-            m_value = 7
+    if m == "normal":
+        m = 0
+    elif m == "bold":
+        m = 1
+    elif m == "underline":
+        m = 4
+    elif m == "blinking":
+        m = 5
+    elif m == "reverse video":
+        m = 7
 
-    if m_value not in [0, 1, 2, 5, 7]:
-        raise ValueError("Invalid mode")
+    print(f"\033[{m}m", end='')
 
-    print(f"\033[{m_value}m", end='')
+
+def unit_test():
+    """ Simple unit testing of crt module code and of a terminal. """
+    print()
+    print("crt module unit test.")
+    print()
+    input("Press Enter to crt.clear() without moving cursor: ")
+    clear()
+
+    input("Press Enter to crt.clear_and_home(): ")
+    clear_and_home()
+
+    # print("01 Top left row of screen (row=0 column=0)")
+    for i in range(1, 10):
+        print(f"Line {i:02d}")
+
+    print()
+    input("Press Enter to crt.home() without clear: ")
+    home()
+    print("THIS IS HOME! ", end='')
+
+    input("Press Enter for crt.goto() test.")
+    clear()
+    for row in range(1, 10):
+        col = row*2 - 1
+        goto(row, col)
+        print(f"X crt.goto(row={row}, col={col})  ")
+    print()
+
+    input("Press Enter for crt.mode() test:")
+    print()
+    unit_test_mode("normal")
+    unit_test_mode("bold")
+    unit_test_mode("underline")
+    unit_test_mode("blinking")
+    unit_test_mode("reverse video")
+    for m in range(10):
+        unit_test_mode(m)
+
+    print("Some terminals support mode values as high as 100")
+    print()
+    print("module crt unit test DONE!")
+
+
+def unit_test_mode(m):
+    print(f"This is ", end='')
+    mode(m)
+    print(f"crt.mode({m!r}) Testing 123...")
+    mode()
+
+
+if __name__ == '__main__':
+    unit_test()
