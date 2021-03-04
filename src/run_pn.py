5c3a5139ebbe612d04fabad204b757bb94b71967
curtwelch <curt@kcwc.com>
2021-03-04 10:47:20 -0500

    experimenting.

--- src/run_pn.py
+++ src/run_pn.py
@@ -10,16 +10,16 @@ import physics_sim as ps
 
 def main():
     """ Simple test of a Proton and a fake Neutron. """
-    sim = ps.Simulation(title="PN Test nc 1.1", dt_max=2e-20)
+    sim = ps.Simulation(title="PN Test nc 2.0", dt_max=2e-20)
 
     v = .010 * ps.CONST_C   # C is the speed of light.
 
     sim.add_p_a((0.0, 0.0, 0.0), v=(0.0, -v/100, 0.0))
-    sim.add_n_a((0.2, 0.0, 0.0), v=(0.0, 0.000, 0.0), nc=1.1)
+    sim.add_n_a((0.2, 0.0, 0.0), v=(0.0, 0.000, 0.0), nc=2.0)
     sim.add_p_a((0.4, 0.0, 0.0), v=(0.0, v/100, 0.0))
 
-    # sim.add_e_a((0.8, 0.0, 0.0), v=(0.0, v, 0.0))
-    # sim.add_e_a((-0.4, 0.0, 0.0), v=(0.0, -v, 0.0))
+    sim.add_e_a((0.8, 0.0, 0.0), v=(0.0, v, 0.0))
+    sim.add_e_a((-0.4, 0.0, 0.0), v=(0.0, -v, 0.0))
 
     sim.run()
 
